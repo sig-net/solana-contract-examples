583de7259fe8052fa7db57386b1dc08e59d6a97d
Pessina <fs.pessina@gmail.com>
2026-01-17 20:24:41 +0700

    Fix withdraw flow

--- frontend/lib/contracts/dex-contract.ts
+++ frontend/lib/contracts/dex-contract.ts
@@ -184,7 +184,7 @@ export class DexContract {
       )
       .accounts({
         authority,
-        feePayer: this.wallet.publicKey,
+        feePayer: authority,
         instructions: SYSVAR_INSTRUCTIONS_PUBKEY,
       })
       .rpc();
